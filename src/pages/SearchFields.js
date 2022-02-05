3559576e708ea93840115ce4ed316cbfd1942ddd
Vince Momot <vincemomot@gmail.com>
2022-02-05 13:09:59 -0500

    homepage now renders the searchbars and flight options

---
+++ src/pages/SearchFields.js
@@ -0,0 +1,20 @@
+// Search Fields 
+import React from 'react';
+
+function SearchFields() {
+  return <div>
+      <div id="searchFields" className="slide-down">
+  <input className="shadow-pop-br" placeholder="ATLANTA" id="origin" />
+
+  <input className="shadow-pop-br" placeholder="DESTINATION" id="destination" />
+
+  <button className="shadow-pop-br" id="searchBtn">
+    <h1>SEARCH</h1>
+  </button>
+</div>
+
+  </div>;
+}
+
+export default SearchFields;
+
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,9 +1,14 @@
 import FlyMap from "../components/Map";
+import Results from "./Results";
+import SearchFields from "./SearchFields";
 
 function SearchResults() {
   return (
     <div>
+      <Results/>
+         <SearchFields/>
       <FlyMap />
+   
     </div>
   );
 }
