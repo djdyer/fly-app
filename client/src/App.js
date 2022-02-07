31c78f3845b55504a79c4d2e6389bda429465df7
Julius Markauskas <juliusm9791@gmail.com>
2022-02-06 20:24:51 -0500

    dded routes on main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
 import Home from "./pages/Home";
 import Navigation from "./pages/Navigation";
+
+
 
 function App() {
   return (
     <div>
-      <Navigation />
+      <Router>
+
+        <Navigation />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          {/* <Route exact path='/search' component={Search} />
+          <Route exact path='/settings' component={Settings} />
+          <Route exact path='/payment' component={Payment} />
+          <Route exact path='/documents' component={Documents} />
+          <Route exact path='/more' component={More} />
+          <Route exact path='/notifications' component={Notifications} />
+          <Route exact path='/filter' component={Filter} /> */}
+          <Route
+            render={function () {
+              return <h1>Not Found</h1>;
+            }}
+          />
+        </Switch>
+
+      </Router>
+
       <Home />
+
+
+
+
+
+
+
     </div>
   );
 }
