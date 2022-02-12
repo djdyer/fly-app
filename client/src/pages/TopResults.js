5dce689b3b6304584499f3cc681cf8ee8b65b689
David Dyer <davidrossdyer@gmail.com>
2022-02-12 18:59:25 -0500

    fixed style things

--- client/src/pages/TopResults.js
+++ client/src/pages/TopResults.js
@@ -6,25 +6,30 @@ import { QUERY_AUCTIONS } from "../utils/queries";
 function TopResults() {
   const { loading, data } = useQuery(QUERY_AUCTIONS);
 
-  const oneDay = 24*60*60*1000;
-if (loading) {
-  return <p>Loading ....</p>
-} else {
-  console.log(data.auctions)
-  return (<>
-      <div className="topResultsContainer">
-        <div id="topResultsHeader">
-          <h2>LEAVING SOON:</h2>
+  const oneDay = 24 * 60 * 60 * 1000;
+  if (loading) {
+    return <p>Loading ....</p>;
+  } else {
+    console.log(data.auctions);
+    return (
+      <>
+        <div className="topResultsContainer">
+          <div id="topResultsHeader">
+            <h2>DEPARTING SOON:</h2>
+          </div>
+          <div className="topResults">
+            {data.auctions.map((auction) => {
+              if (
+                auction.auctionEndDate > new Date() &&
+                auction.auctionEndDate < +new Date() + oneDay
+              ) {
+                return <Auction key={auction._id} auction={auction} />;
+              }
+            })}
+          </div>
         </div>
-        <div className="topResults">
-          {data.auctions.map((auction) => {
-            if (auction.auctionEndDate > (new Date()) && auction.auctionEndDate < (+new Date() + oneDay)){
-            return <Auction key={auction._id} auction={auction} />;
-            } 
-          })}
-        </div>
-      </div>
-  </>);
-}
+      </>
+    );
+  }
 }
 export default TopResults;
