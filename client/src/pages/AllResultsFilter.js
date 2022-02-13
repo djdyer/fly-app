e010faa42375500ffd9e450b809ec1a59902ad1a
David Dyer <davidrossdyer@gmail.com>
2022-02-13 12:17:44 -0500

    fixed style things, removed profile nav

--- client/src/pages/AllResultsFilter.js
+++ client/src/pages/AllResultsFilter.js
@@ -107,9 +107,11 @@ export default function AllResultsFilter() {
         <div id="resultsHeader">
           <h2>All Auctions:</h2>
         </div>
-        {data?.auctions.map((auction) => {
-          return <Auction key={auction._id} auction={auction} />;
-        })}
+        <div>
+          {data?.auctions.map((auction) => {
+            return <Auction key={auction._id} auction={auction} />;
+          })}
+        </div>
       </div>
     </div>
   );
--- client/src/pages/AuctionDetail.js
+++ client/src/pages/AuctionDetail.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import AuctionMap from "../components/AuctionMap";
-import AuctionButton from "../components/AuctionButton"
-import Auth from '../utils/auth';
+import AuctionButton from "../components/AuctionButton";
+import Auth from "../utils/auth";
 
 import { useQuery, useMutation } from "@apollo/client";
 import { UPDATE_BID, SAVE_FLIGHT } from "../utils/mutations";
@@ -9,7 +9,6 @@ import { QUERY_AUCTION } from "../utils/queries";
 import Timer from "../components/Timer";
 
 function AuctionDetail() {
-  
   const watch = require("../../src/assets/icons/watch.png");
   const watchHover = require("../../src/assets/icons/watch2.png");
   const plane = require("../../src/assets/icons/plane.png");
@@ -20,123 +19,124 @@ function AuctionDetail() {
   });
   const auctionData = data?.auction || {};
 
-
-
-
-  return ( 
+  return (
     <div>
-      {loading ? ( <p>Loading .....</p> ) : (
-      <div className="auction">
-        <div className="auctionDetail">
-          <div id="myPlane"> 
-          <img
-            alt="plane"
-            className="plane"
-            src={`/images/planes/${auctionData.image}`}
-          />
-          </div>
-
-          <div id="auctionHeader">
-            <div id="trip">
-              <h2>{auctionData.origin}</h2>
-              <img className="planeIcon" alt="plane icon" src={plane} />
-              <h2>{auctionData.destination}</h2>
+      {loading ? (
+        <p>Loading .....</p>
+      ) : (
+        <div className="auction">
+          <div className="auctionDetail">
+            <div id="myPlane">
+              <img
+                alt="plane"
+                className="plane"
+                src={`/images/planes/${auctionData.image}`}
+              />
             </div>
-            <div id="countdown"><Timer auctionEndDate={+auctionData.auctionEndDate} /></div>
-          </div>
-          <AuctionMap />
-          <div id="auctionBody">
-            <div className="auctionBodyColumn">
-              <div className="auctionRow">
-                <h3>Date:</h3>
-                {(new Date(+auctionData.flightDate).toLocaleDateString())}
-              </div>
-              <div className="auctionRow">
-                <h3>Time:</h3>
-                {(new Date(+auctionData.flightDate).toLocaleTimeString())}
-              </div>
-              <div className="auctionRow">
-                <h3>Aircraft:</h3>
-                {auctionData.aircraft}
-              </div>
 
-              <div className="auctionRow">
-                <h3>Flight Num:</h3>
-                {auctionData.flightNum}
+            <div id="auctionHeader">
+              <div id="trip">
+                <h2>{auctionData.origin}</h2>
+                <img className="planeIcon" alt="plane icon" src={plane} />
+                <h2>{auctionData.destination}</h2>
               </div>
-
-              <div className="auctionRow">
-                <h3>Cabin Size:</h3>
-                {auctionData.cabinSize}
-              </div>
-
-              <div className="auctionRow">
-                <h3>Operator:</h3>
-                {auctionData.operator}
+              <div id="countdown">
+                <Timer auctionEndDate={+auctionData.auctionEndDate} />
               </div>
             </div>
-            <div className="auctionBodyColumn">
-              <div className="leadingBidContainer">
-                Leading Bid:
-                <div id="leadingBid">
-                  <h2>${auctionData.currentBid}</h2>
+            <AuctionMap />
+            <div id="auctionBody">
+              <div className="auctionBodyColumn">
+                <div className="auctionRow">
+                  <h3>Date:</h3>
+                  {new Date(+auctionData.flightDate).toLocaleDateString()}
+                </div>
+                <div className="auctionRow">
+                  <h3>Time:</h3>
+                  {new Date(+auctionData.flightDate).toLocaleTimeString()}
+                </div>
+                <div className="auctionRow">
+                  <h3>Aircraft:</h3>
+                  {auctionData.aircraft}
+                </div>
+
+                <div className="auctionRow">
+                  <h3>Flight Num:</h3>
+                  {auctionData.flightNum}
+                </div>
+
+                <div className="auctionRow">
+                  <h3>Cabin Size:</h3>
+                  {auctionData.cabinSize}
+                </div>
+
+                <div className="auctionRow">
+                  <h3>Operator:</h3>
+                  {auctionData.operator}
                 </div>
               </div>
-              <div className="termsContainer">
-                <input type="checkbox" id="termsConfirm"></input>
-                <h3>
-                  Agree to <a href="/termsandcondtions">Terms+Conditions</a>
-                  {auctionData.termsConfirm}
-                </h3>
+              <div className="auctionBodyColumn">
+                <div className="leadingBidContainer">
+                  Leading Bid:
+                  <div id="leadingBid">
+                    <h2>${auctionData.currentBid}</h2>
+                  </div>
+                </div>
+                <div className="termsContainer">
+                  <input type="checkbox" id="termsConfirm"></input>
+                  <h3>
+                    Agree to <a href="/termsandcondtions">Terms+Conditions</a>
+                    {auctionData.termsConfirm}
+                  </h3>
+                </div>
+                <div className="serviceContainer">
+                  Service Detail:
+                  <a id="addServiceLink" href="/servicedetail">
+                    Read More
+                  </a>
+                </div>
+                <li>In-flight attendence available.</li>
+                <li>Bar stocked with non-alcoholic beverages.</li>
               </div>
-              <div className="serviceContainer">
-                Service Detail:
-                <a id="addServiceLink" href="/servicedetail">
-                  Read More
-                </a>
+            </div>
+            <div className="bidHistory">
+              <div id="bidHeader">Bid History</div>
+              <div className="otherBid">
+                <h5>Username</h5>
+                <h5>Time Stamp</h5>
+                <h5>Bid Amount</h5>
               </div>
-              <li>In-flight attendence available.</li>
-              <li>Bar stocked with non-alcoholic beverages.</li>
+              <div className="otherBid">
+                <h5>Username</h5>
+                <h5>Time Stamp</h5>
+                <h5>Bid Amount</h5>
+              </div>
+              <div className="otherBid">
+                <h5>Username</h5>
+                <h5>Time Stamp</h5>
+                <h5>Bid Amount</h5>
+              </div>
             </div>
+            <div className="watchOption">
+              <a href="/watchlist">
+                <img
+                  id="watchIcon"
+                  className="icon default"
+                  alt="watch"
+                  src={watch}
+                />
+                <img
+                  id="watchIcon2"
+                  className="icon hover"
+                  alt="watch hover"
+                  src={watchHover}
+                />
+              </a>
+              <h2>Watch this Auction </h2>
+            </div>
+            <AuctionButton auctionData={auctionData} />
           </div>
-          <div className="bidHistory">
-            <div id="bidHeader">Bid History</div>
-            <div className="otherBid">
-              <h5>Username</h5>
-              <h5>Time Stamp</h5>
-              <h5>Bid Amount</h5>
-            </div>
-            <div className="otherBid">
-              <h5>Username</h5>
-              <h5>Time Stamp</h5>
-              <h5>Bid Amount</h5>
-            </div>
-            <div className="otherBid">
-              <h5>Username</h5>
-              <h5>Time Stamp</h5>
-              <h5>Bid Amount</h5>
-            </div>
-          </div>
-          <div className="watchOption">
-            <a href="/watchlist">
-              <img
-                id="watchIcon"
-                className="icon default"
-                alt="watch"
-                src={watch}
-              />
-              <img
-                id="watchIcon2"
-                className="icon hover"
-                alt="watch hover"
-                src={watchHover}
-              />
-            </a>
-            <h3>Watch this Auction </h3>
-          </div>
-          <AuctionButton  auctionData={auctionData}/>
         </div>
-      </div>
       )}
     </div>
   );
