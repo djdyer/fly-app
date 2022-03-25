a4f607ae0b1def3f8a562ebbe0c326af049ec9c0
Julius Markauskas <juliusm9791@gmail.com>
2022-03-24 20:22:02 -0400

    finised search filter

--- client/src/utils/allResultsFilterSearch.js
+++ client/src/utils/allResultsFilterSearch.js
@@ -1,70 +1,14 @@
-export default function searchFilterData(auctionsData, filter, loading) {
-    if (loading) {
-        return;
-    } else {
-        const filterOriginIds = [];
-        const filterDestinationIds = [];
-        const dateDestinationIds = [];
-        const operatorIds = [];
-        const aircraftIds = [];
-        const cabinSizeIds = [];
+export default function searchFilterData(auctionsData, filter) {
 
+    let filtered = [...auctionsData];
 
-        auctionsData.forEach(obj => {
-            if (obj.origin.toLowerCase() === filter.filterOrigin.toLowerCase()) {
-                filterOriginIds.push(obj._id);
-            }
-            if (obj.destination.toLowerCase() === filter.filterDestination.toLowerCase()) {
-                filterDestinationIds.push(obj._id);
-            }
-            if (obj.flightDate.toLowerCase() === filter.dateDestination.toLowerCase()) {
-                dateDestinationIds.push(obj._id);
-            }
-            if (obj.operator.toLowerCase() === filter.operator.toLowerCase()) {
-                operatorIds.push(obj._id);
-            }
-            if (obj.aircraft.toLowerCase() === filter.aircraft.toLowerCase()) {
-                aircraftIds.push(obj._id);
-            }
-            if (obj.cabinSize.toLowerCase() === filter.cabinSize.toLowerCase()) {
-                cabinSizeIds.push(obj._id);
-            }
-        });
-        const SearchFieldsIdArrays = {
-            a: filter.filterOrigin === "" ? false : filterOriginIds,
-            b: filter.filterDestination === "" ? false : filterDestinationIds,
-            c: filter.dateDestination === "" ? false : dateDestinationIds,
-            d: filter.operator === "" ? false : operatorIds,
-            e: filter.aircraft === "" ? false : aircraftIds,
-            f: filter.cabinSize === "" ? false : cabinSizeIds
-        }
-        const noResultinOneSearchLine = Object.values(SearchFieldsIdArrays).forEach((value) => {
-            console.log(value.length === 0)
-            if (value.length === 0) {
-                return false;
-            }
-        });
-
-        if (!noResultinOneSearchLine) {
-            const justFilledSearchFields = []
-            Object.values(SearchFieldsIdArrays).forEach((value) => {
-                if (value) {
-                    justFilledSearchFields.push(value);
-                }
-            });
-
-            // combine multiple arrays into one
-            const oneArrayOfAllsearch = Array.prototype.concat.apply([], justFilledSearchFields);
-
-            // leaves just unique values in array
-            const afterSearchIds = oneArrayOfAllsearch.filter(function (item, pos, self) {
-                return self.indexOf(item) == pos;
-            })
-
-            console.log(oneArrayOfAllsearch)
-            console.log(afterSearchIds)
-
-            return afterSearchIds;
-        } else return [];
-    }
+    filtered = filtered
+        .filter(auction => auction.origin.toLowerCase().indexOf(filter.filterOrigin.toLowerCase()) !== -1)
+        .filter(auction => auction.destination.toLowerCase().indexOf(filter.filterDestination.toLowerCase()) !== -1)
+        .filter(auction => auction.flightDate.toString().indexOf(filter.dateDestination.toString()) !== -1)
+        .filter(auction => auction.operator.toLowerCase().indexOf(filter.operator.toLowerCase()) !== -1)
+        .filter(auction => auction.aircraft.toLowerCase().indexOf(filter.aircraft.toLowerCase()) !== -1)
+        .filter(auction => auction.cabinSize.toLowerCase().indexOf(filter.cabinSize.toLowerCase()) !== -1)
+        
+    return filtered;
 };
